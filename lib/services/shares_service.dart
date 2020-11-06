7d24aa1dd55128ebb54a0e99bae006bf18b74afd
Jonty <jonty.morris@yahoo.co.nz>
2020-11-07 00:38:14 +1300

    Connected to daily api

--- lib/services/shares_service.dart
+++ lib/services/shares_service.dart
@@ -1,13 +1,22 @@
 import 'dart:convert';
 import 'package:StocksApp/model/share.dart';
+import 'package:StocksApp/model/candle.dart';
 import 'package:http/http.dart' as http;
 import '../env.dart';
 
-const _source = baseUrl + 'api/shares';
+const _sharesSource = baseUrl + 'api/shares';
+const _dailySource = baseUrl + 'api/daily/';
 
 Future<List<Share>> getShares() async {
-  var response = await http.get(_source);
+  var response = await http.get(_sharesSource);
   Iterable shares = jsonDecode(response.body);
 
   return shares.map((e) => Share.fromJson(e)).toList();
+}
+
+Future<List<Candle>> getDaily(String symbol) async {
+  var response = await http.get(_dailySource + symbol);
+  Iterable candles = jsonDecode(response.body);
+
+  return candles.map((e) => Candle.fromJson(e)).toList();
 }
