d5c0571fb840eee257bef7de4d2fa3227e1eea77
Jonty <jonty.morris@yahoo.co.nz>
2020-11-07 17:35:25 +1300

    Added search bar

--- lib/widgets/shares_widget.dart
+++ lib/widgets/shares_widget.dart
@@ -12,11 +12,13 @@ class SharesWidget extends StatefulWidget {
 }
 
 class _SharesState extends State<SharesWidget> {
+  String _searchQuery;
   Future<List<Share>> _futureShares;
 
   @override
   void initState() {
     super.initState();
+    _searchQuery = '';
     _futureShares = getShares();
   }
 
@@ -26,28 +28,74 @@ class _SharesState extends State<SharesWidget> {
         future: _futureShares,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            List<Widget> listItems = [
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Text(
-                  'SHARE LISTINGS',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-              )
+            var children = [
+              _buildHeader(),
+              _buildFilter(),
             ];
 
-            listItems.addAll(snapshot.data.map((e) => _buildShare(e)));
+            var shares = _sortAndFilterShares(snapshot.data);
+            children.addAll(shares.map((e) => _buildShare(e)));
 
             return ListView(
-              children: listItems,
+              children: children,
             );
           }
 
           return LoadingWidget();
         });
+  }
+
+  Widget _buildHeader() {
+    return Padding(
+      padding: EdgeInsets.only(left: 20, top: 20, right: 20),
+      child: Text(
+        'SHARE LISTINGS',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFilter() {
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: TextField(
+        autofocus: false,
+        onChanged: (value) => {
+          setState(() {
+            this._searchQuery = value;
+          })
+        },
+        decoration: InputDecoration(hintText: 'Filter by name'),
+      ),
+    );
+  }
+
+  List<Share> _sortAndFilterShares(List<Share> shares) {
+    shares.sort((a, b) => a.name.compareTo(b.name));
+
+    var keywords = this
+        ._searchQuery
+        .toLowerCase()
+        .trim()
+        .split(' ')
+        .where((e) => e.length > 0)
+        .toList();
+
+    if (keywords.length == 0) return shares;
+
+    return shares.where((e) => _containsKeyword(e, keywords)).toList();
+  }
+
+  bool _containsKeyword(Share share, List<String> keywords) {
+    var name = share.name.toLowerCase();
+    for (var keyword in keywords) {
+      if (!name.contains(keyword)) return false;
+    }
+
+    return true;
   }
 
   Widget _buildShare(Share share) {
